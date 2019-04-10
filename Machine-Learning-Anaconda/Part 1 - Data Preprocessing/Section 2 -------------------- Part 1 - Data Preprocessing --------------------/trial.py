595f82a2bf3d3e903460042ecb1689f8fe8ff06e
sauravsb99 <sauravsb99@gmail.com>
2019-04-11 03:24:29 +0530

    End of day 1

--- Machine-Learning-Anaconda/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/trial.py
+++ Machine-Learning-Anaconda/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/trial.py
@@ -2,3 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+dataset = pd.read_csv('Data.csv')
+print("HI")
+
+X = dataset.iloc[:, :-1].values
+
+# print(X)
+
+Y = dataset.iloc[:, 3].values
+
+print(Y)
