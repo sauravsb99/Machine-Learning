7987e3b410321e91883f5357a2f0d9b9f00b016f
Saurav S B <32162836+sauravsb99@users.noreply.github.com>
2019-05-18 00:50:33 +0530

    part 1 complete

--- Machine Learning/Part 1 - Data Preprocessing/temp.py
+++ Machine Learning/Part 1 - Data Preprocessing/temp.py
@@ -12,3 +12,7 @@ dataset = pd.read_csv("Data.csv")
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+from sklearn.preprocessing import Imputer
+imputer=Imputer(missing_values = "NaN" , strategy = "mean" , axis = 0)
+imputer = imputer.fit(x[: , 1:3])
+x[: , 1:3] = imputer.transform(x[: , 1:3])
